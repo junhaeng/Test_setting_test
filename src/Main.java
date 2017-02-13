d46ab04e96b5366e874bd0fbe2d57fc39a3bcab6
jhcorea <jhcorea@tmon.co.kr>
2017-02-13 17:28:23 +0900

    local Test Commit2

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] srgs){
         System.out.println("Test!");
         System.out.println("Local Commit Test!");
+        System.out.println("Local Commit Test2!");
+
     }
 }
